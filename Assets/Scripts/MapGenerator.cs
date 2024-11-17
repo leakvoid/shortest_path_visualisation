966ee23d70ba9a1a988d22bb9275f574c4678c62
Dmitry <leakvoid@gmail.com>
2024-11-17 23:54:46 +0300

    maze generation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
@@ -18,17 +19,7 @@ public class MapGenerator : MonoBehaviour
     }
     CellType[,] mapCells;
 
-    public CellType[,] GetMapData()
-    {
-        return mapCells;
-    }
-
-    void Start()
-    {
-        GenerateNewMap();
-    }
-
-    void GenerateNewMap()
+    public CellType[,] GenerateNewMap()
     {
         mapCells = new CellType[mapSize, mapSize];
 
@@ -75,6 +66,8 @@ public class MapGenerator : MonoBehaviour
                 j++;
             }
         }
+
+        return mapCells;
     }
 
     void DebugPrint()
@@ -91,8 +84,86 @@ public class MapGenerator : MonoBehaviour
         Debug.Log(line);
     }
 
-    void ShortestPath()
+    // shortest path section
+    Stack<int> shortestPath;
+
+    public int[] FindShortestPath()
     {
+        int i = 0;
+        while (mapCells[0,i] != CellType.MapStart)
+            i++;
+
+        var currentPath = new Stack<int>();
+        currentPath.Push(i);
+        var distanceMemory = new Dictionary<(int, int), int>();
+        distanceMemory[(0,i)] = 0;
+        TraverseToNextLayer(2, currentPath, distanceMemory);
+
+        return shortestPath.ToArray();
+    }
+
+    void TraverseToNextLayer(int layer, Stack<int> path, Dictionary<(int, int), int> memory)
+    {
+        if (layer == mapCells.GetLength(0) - 1)
+            HandleFinalLayer(layer, path, memory);
+        else
+        HandleIntermediateLayer(layer, path, memory);
+    }
+
+    void HandleIntermediateLayer(int layer, Stack<int> path, Dictionary<(int, int), int> memory)
+    {
+        int lastExit = path.Peek();
+
+        // search first exit directly above or up
+        for (int i = lastExit; i < mapCells.GetLength(1); i++)
+        {
+            if (mapCells[layer, i] == CellType.WallExit)
+            {
+                int currentDistance = memory[(layer - 2, lastExit)] + System.Math.Abs(lastExit - i);
+                if (!memory.ContainsKey((layer, i)) || currentDistance < memory[(layer, i)])
+                {
+                    memory[(layer, i)] = currentDistance;
+                    path.Push(i);
+                    TraverseToNextLayer(layer + 2, path, memory);
+                    path.Pop();
+                }
+                break;
+            }
+        }
+
+        // search first exit down
+        for (int i = lastExit - 1; i > 0; i--)
+        {
+            if (mapCells[layer, i] == CellType.WallExit)
+            {
+                int currentDistance = memory[(layer - 2, lastExit)] + System.Math.Abs(lastExit - i);
+                if (!memory.ContainsKey((layer, i)) || currentDistance < memory[(layer, i)])
+                {
+                    memory[(layer, i)] = currentDistance;
+                    path.Push(i);
+                    TraverseToNextLayer(layer + 2, path, memory);
+                    path.Pop();
+                }
+                break;
+            }
+        }
+    }
+
+    void HandleFinalLayer(int layer, Stack<int> path, Dictionary<(int, int), int> memory)
+    {
+        int i = 0;
+        while (mapCells[layer, i] != CellType.MapExit)
+            i++;
         
+        int lastExit = path.Peek();
+        int currentDistance = memory[(layer - 2, lastExit)] + System.Math.Abs(lastExit - i);
+        if (memory.ContainsKey((layer, i)) && currentDistance >= memory[(layer, i)])
+            return;
+
+        memory[(layer, i)] = currentDistance;
+
+        path.Push(i);
+        shortestPath = new Stack<int>(path);
+        path.Pop();
     }
 }
--- Assets/Scripts/MapRenderer.cs
+++ Assets/Scripts/MapRenderer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -7,8 +8,10 @@ public class MapRenderer : MonoBehaviour
 {
     [SerializeField] Tilemap roomTilemap;
     [SerializeField] Tilemap backgroundTilemap;
+
     [SerializeField] RuleTile wallTile;
     [SerializeField] RuleTile backgroundTile;
+    [SerializeField] RuleTile pathTile;
 
     MapGenerator mapGenerator;
 
@@ -19,14 +22,20 @@ public class MapRenderer : MonoBehaviour
 
     void Start()
     {
-        MapGenerator.CellType[,] mapData = mapGenerator.GetMapData();
+        DrawNewMap();
+    }
+
+    public void DrawNewMap()
+    {
+        backgroundTilemap.ClearAllTiles();
+        roomTilemap.ClearAllTiles();
+
+        MapGenerator.CellType[,] mapData = mapGenerator.GenerateNewMap();
 
         for (int i = 0; i < mapData.GetLength(0); i++)
         {
             for (int j = 0; j < mapData.GetLength(1); j++)
-            {
                 backgroundTilemap.SetTile(new Vector3Int(i, j, 0), backgroundTile);
-            }
         }
 
         for (int i = 0; i < mapData.GetLength(0); i++)
@@ -34,10 +43,20 @@ public class MapRenderer : MonoBehaviour
             for (int j = 0; j < mapData.GetLength(1); j++)
             {
                 if (mapData[i,j] == MapGenerator.CellType.Wall)
-                {
                     roomTilemap.SetTile(new Vector3Int(i, j, 0), wallTile);
-                }
             }
+        }
+
+        int[] shortestPath = mapGenerator.FindShortestPath();
+        for (int i = 0; i < shortestPath.Length; i++)
+            roomTilemap.SetTile(new Vector3Int(i * 2, shortestPath[i], 0), pathTile);
+        
+        for (int i = 0; i < shortestPath.Length - 1; i++)
+        {
+            int start = System.Math.Min(shortestPath[i], shortestPath[i + 1]);
+            int end = System.Math.Max(shortestPath[i], shortestPath[i + 1]);
+            for (int j = start; j <= end; j++)
+                roomTilemap.SetTile(new Vector3Int(i * 2 + 1, j, 0), pathTile);
         }
     }
 }
